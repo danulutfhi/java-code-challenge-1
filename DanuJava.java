3add243416683bcf0e20784dafeba3fbc6a9a4c4
danulutfhi <91659938+danulutfhi@users.noreply.github.com>
2021-10-13 02:13:40 +0700

    Add files via upload

---
+++ DanuJava.java
@@ -0,0 +1,27 @@
+public class DanuJava {
+    public static void main(String[] args) {
+        // membuat nama alamat dari peserta
+        // first name, lastname, age, phone number, address, and other
+        //system.out.println("Hello World");
+
+        // Inisialisasi
+        String first_name = "";
+        String last_name = "";
+        String phone_number;
+        String address = "";
+        int age = 0;
+
+        // Input variable
+        first_name = "Danu";
+        last_name = "Lutfhi";
+        phone_number = "082172255841";
+        address = "Pekanbaru";
+        age = 21;
+
+        System.out.println("First Name : " + first_name);
+        System.out.println("Last Name : " + last_name);
+        System.out.println("Phone Number : " + phone_number);
+        System.out.println("Address : " + address);
+        System.out.println("Age : " + age);
+    }    
+}
